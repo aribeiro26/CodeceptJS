3ae87fb9a849964cc96b76dbd2d60ce2df578731
aribeiro26 <73800428+aribeiro26@users.noreply.github.com>
2021-11-18 02:26:30 -0300

    condecept initialize

---
+++ codecept.conf.js
@@ -0,0 +1,36 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://eventoexpert.xpi.com.br',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'CodeceptJS',
+  translation: 'pt-BR',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
